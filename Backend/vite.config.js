38fee329d299d07536274ee6c0d7ee585e499b6a
userlg <guipeluis90@gmail.com>
2025-07-17 23:40:22 -0400

    New changes

--- Backend/vite.config.js
+++ Backend/vite.config.js
@@ -1,13 +1,31 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import javascriptObfuscator from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
+        process.env.NODE_ENV === "production" &&
+            javascriptObfuscator({
+                compact: true,
+                controlFlowFlattening: true,
+                deadCodeInjection: true,
+                stringArray: true,
+                stringArrayEncoding: ["base64"],
+            }),
     ],
+    build: {
+        minify: "terser",
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+    },
 });
